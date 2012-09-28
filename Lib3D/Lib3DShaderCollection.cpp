f0855a3268e2bfb05e0e633baf4a2d3cf89ed0f7
Charles Augustin Barto <bartoc@caen-vnc08.engin.umich.edu>
2012-09-27 21:04:46 -0400

    t from CAIN

---
+++ Lib3D/Lib3DShaderCollection.cpp
@@ -0,0 +1,31 @@
+#include "StdAfx.h"
+#include "Lib3DShaderCollection.h"
+
+Lib3DShaderCollection::Lib3DShaderCollection(void)
+{
+}
+
+
+Lib3DShaderCollection::~Lib3DShaderCollection(void)
+{
+}
+
+#pragma region accessors
+shared_ptr<Lib3DPixelShader> Lib3DShaderCollection::getPS()
+{
+	return m_ps;
+}
+void Lib3DShaderCollection::setPS(shared_ptr<Lib3DPixelShader> ps)
+{
+	m_ps = ps;
+}
+
+shared_ptr<Lib3DVertexShader> Lib3DShaderCollection::getVS()
+{
+	return m_vs;
+}
+void Lib3DShaderCollection::setVS(shared_ptr<Lib3DVertexShader> vs)
+{
+	m_vs = vs;
+}
+#pragma endregion
