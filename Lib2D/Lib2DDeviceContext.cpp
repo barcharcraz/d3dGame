70f69425e571709fa54ff3c2f1e4109805000e1e
Charlie Barto <bartoc@umich.edu>
2012-10-09 13:31:45 -0400

    builds successfully

---
+++ Lib2D/Lib2DDeviceContext.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Lib2DDeviceContext.h"
+
+
+Lib2DDeviceContext::Lib2DDeviceContext(void)
+{
+}
+
+
+Lib2DDeviceContext::~Lib2DDeviceContext(void)
+{
+}
---
+++ Lib2D/Lib2DDeviceContext.h
@@ -0,0 +1,8 @@
+#pragma once
+class Lib2DDeviceContext
+{
+public:
+	Lib2DDeviceContext(void);
+	~Lib2DDeviceContext(void);
+};
+
