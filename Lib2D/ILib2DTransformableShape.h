37746bcbd98002ead96bb90533fe1788a62a53cc
Charlie Barto <bartoc@umich.edu>
2012-12-13 16:38:16 -0500

    from lovelace
    not really sure what is in here

---
+++ Lib2D/ILib2DTransformableShape.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "ILib2DTransformableObject.h"
+#include "ILib2DShape.h"
+
+class ILib2DTransformableShape : public ILib2DShape, public ILib2DTransformableObject {
+};
--- Lib2D/Lib2DAnimatedShape.h
+++ Lib2D/Lib2DAnimatedShape.h
@@ -2,9 +2,17 @@
 #include "ILib2DAnimation.h"
 #include "ILib2DShape.h"
 #include "Lib2DShape.h"
+#include "ILib2DTransformableShape.h"
+#include <memory>
 class Lib2DAnimatedShape : public ILib2DShape {
 public:
-    Lib2DAnimatedShape(ILib2DAnimation *animation, ILib2DShape *shape);
+    Lib2DAnimatedShape(ILib2DAnimation *animation, ILib2DTransformableShape *shape);
+    virtual void draw(ID2D1DeviceContext* pContext) override;
+    void update(double dt);
 private:
+    double m_time;
+    double m_start;
+    const D2D1::Matrix3x2F m_initial;
     ILib2DAnimation* m_animation;
+    ILib2DTransformableShape* m_shape;
 };
