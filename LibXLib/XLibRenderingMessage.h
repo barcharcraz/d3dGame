70a2b7e4733a57a9de98666ab8c63ad41856ccfb
Charlie Barto <barcharcraz@msn.com>
2013-04-14 19:10:30 -0400

    adding Xlib support:

---
+++ LibXLib/XLibRenderingMessage.h
@@ -0,0 +1,12 @@
+#ifndef LIBXLIB_XLIBRENDERINGMESSAGE_H
+#define LIBXLIB_XLIBRENDERINGMESSAGE_H
+#include <X11/Xlib.h>
+
+#include <LibCommon/RenderingMessage.h>
+namespace LibXLib {
+	class XLibRenderingMessage : public RenderingMessage {
+		
+	}
+}
+
+#endif
