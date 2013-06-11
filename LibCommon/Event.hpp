2d6fbc276956f02d43805b436599f5fea25ae6b4
Charlie Barto <barcharcraz@msn.com>
2013-06-11 00:02:42 -0400

    added multicast contravariant events! using TMP

--- LibCommon/Event.hpp
+++ LibCommon/Event.hpp
@@ -1,6 +1,25 @@
 #ifndef LIBCOMMON_EVENTS_HPP
 #define LIBCOMMON_EVENTS_HPP
-
-
+#include "stdafx.h"
+#include <vector>
+#include <functional>
+namespace LibCommon {
+template<typename T>
+	class Event<T> {
+	public:
+		template<typename U>
+		int connect<U>(std::function<void(U)> func) {
+			int retval = callbacks.size();
+			callbacks.push_back([](T v){
+				if(dynamic_cast<U>(v)) {
+					func(dynamic_cast<U>(v));
+				}
+			});
+			return retval;
+		}
+	private:
+		std::vector<std::function<void(T)> > callbacks;
+	};
+}
 
 #endif
