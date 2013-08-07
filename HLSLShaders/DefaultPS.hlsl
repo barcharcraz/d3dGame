417d1c45f9fcd8b719bb787642ce30459bc344a7
Charlie Barto <barcharcraz@msn.com>
2013-08-07 17:22:14 -0400

    added prefabs and textures

--- HLSLShaders/DefaultPS.hlsl
+++ HLSLShaders/DefaultPS.hlsl
@@ -1,8 +1,12 @@
+Texture2D tex;
+SamplerState samp;
 struct VS_OUTPUT {
 	float4 Pos : SV_POSITION;
 	float3 uv : TEXCOORD;
 };
 float4 main(VS_OUTPUT input) : SV_TARGET
 {
-	return float4(input.uv.x, input.uv.y, 1.0f, 1.0f);
+	float4 color;
+	color = tex.Sample(samp, input.uv);
+	return color;
 }
