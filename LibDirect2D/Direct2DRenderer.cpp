f9a9adffc7a6fae5f53f5ad2fe4e44f9ad28a5f1
Charlie Barto <barcharcraz@msn.com>
2013-08-20 03:53:02 -0400

    fixed direct2d renderer

--- LibDirect2D/Direct2DRenderer.cpp
+++ LibDirect2D/Direct2DRenderer.cpp
@@ -57,7 +57,7 @@ D2D1_MATRIX_3X2_F Direct2DRenderer::GetProjection() const {
 	if (FAILED(hr)) {
 		throw std::system_error(hr, std::system_category());
 	}
-	auto proj = D2D1::Matrix3x2F::Scale(desc.Width / 2, desc.Height / 2);
+	auto proj = D2D1::Matrix3x2F::Scale(desc.Height / 2, desc.Height / 2);
 	proj = proj * D2D1::Matrix3x2F::Translation(desc.Width / 2, desc.Height / 2);
 	return proj;
 }
