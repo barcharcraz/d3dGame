c8f7895e3d8ff32a785eb51c3470651da99d5c94
Charlie Barto <charlie@westquad-148080.reshall.umich.edu>
2013-11-05 23:05:59 -0500

    added recursive preprocessor

--- Utils/preprocess.h
+++ Utils/preprocess.h
@@ -1,21 +1,48 @@
 #include "files.h"
 #include "strings.h"
 #include <iostream>
+#include <istream>
+#include <sstream>
+#include <fstream>
 #ifndef UTILS_PREPROCESS_H
 #define UTILS_PREPROCESS_H
 
 namespace utils {
     //freestanding parsing functions
-    inline std::string parse_includes(const std::string& filename) {
-        
+    inline std::string preprocess_includes(std::istream& input) {
+        std::ostringstream output;
+        std::string compareTo = "#include ";
+        std::string line = "";
+        while(std::getline(input, line)) {
+            if(line.compare(0, compareTo.size(), compareTo) == 0) {
+                std::string filename = line.substr(compareTo.size(), line.size());
+                trim(&filename);
+                trim(&filename, '"');
+                std::cerr << filename << std::endl;
+                std::ifstream file{filename};
+                output << preprocess_includes(file);
+            }
+        }
+        return output.str();
+    }
+    inline std::string load_source_file(const std::string& filename) {
+        std::ifstream file(filename);
+        return preprocess_includes(file);
+    }
+    inline void add_text(std::string* const self, std::string text, int offset) {
+        if(text.back() != '\n') {
+            text.push_back('\n');
+        }
+        self->insert(offset, std::move(text));
     }
     class SourceFile {
     public:
         SourceFile() = default;
 		explicit SourceFile(int offset_arg) : offset(offset_arg) {}
-        SourceFile(std::vector<std::string> source) 
-            : shader_src(std::move(source))
+        SourceFile(std::string source)
         {
+            std::stringstream instr(source);
+            shader_src = preprocess_includes(instr);
         }
 		int Offset() const {
 			return offset;
@@ -23,31 +50,14 @@ namespace utils {
 		void Offset(int newOffset) {
 			offset = newOffset;
 		}
-        std::vector<const char*> cdata() const {
-            return utils::convert_array(shader_src);
-        }
-        void include_parse() { 
-            std::vector<std::string> includes;
-            std::string compareTo = "#include ";
-			for (auto str = begin(shader_src) + offset; str != end(shader_src); ++str) {
-                if(str->compare(0, compareTo.size(), compareTo) == 0) {
-                    //get the contents of the other file
-                    std::string filename = str->substr(compareTo.size(), str->size());
-                    filename.erase(find(begin(filename), end(filename), '\n'));
-                    //first we trim off the spaces than we trim off the "s, we do not
-                    //support include paths
-                    trim(&filename);
-                    trim(&filename, '"');
-                    std::cerr << filename << std::endl;
-                    *str = utils::slurpText(filename);
-                }
-            }
+        const char* cdata() const {
+            return shader_src.c_str();
         }
         void add_text(std::string text) {
 			if (text.back() != '\n') {
 				text.push_back('\n');
 			}
-            shader_src.insert(begin(shader_src) + offset, std::move(text));
+            shader_src.insert(offset, std::move(text));
         }
         void add_define(std::string name) {
             add_text("#define " + name);
@@ -66,7 +76,7 @@ namespace utils {
             }
         }
     private:
-        std::vector<std::string> shader_src;
+        std::string shader_src;
 		int offset = 0;
     };
 }
