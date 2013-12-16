d7dbbd3564d8d491ae0f40ce917341357b5efbc0
charlie barto <charlie@Dijkstra.(none)>
2013-12-16 00:59:39 -0500

    screwed around with cmakelists

---
+++ ecs.h
@@ -0,0 +1,10 @@
+#ifndef SPARSE_ECS_H
+#define SPARSE_ECS_H
+
+#include "LibECS/Component.h"
+#include "LibECS/Scene.h"
+#include "LibECS/Row.h"
+#include "LibECS/System.h"
+#include "LibECS/World.h"
+
+#endif
